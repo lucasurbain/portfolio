6b7551e8c865bbcca70933e0358f3e7756c85ffd
lucasurbain <lucasurbain68@protonmail.ch>
2022-12-06 18:13:18 +0100

    2022.9.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,3 +134,12 @@ let swiperProjects = new Swiper(".projects__container", {
         }
     },
 });
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
