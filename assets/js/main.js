82de17404d94db5cdfdcec161268c53ab5bd8de5
lucasurbain <lucasurbain68@protonmail.ch>
2023-01-01 10:41:53 +0100

    2023.10.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,7 +146,7 @@ window.addEventListener('scroll', scrollTop)
 
 /*=============== GET VERSION ===============*/
 function getVersion() {
-    var version = "2022.10.1";
+    var version = "2023.10.2";
     var copyright = " | Copyright &#169; 2022 Lucas Urbain | All Rigths Reserved.";
     var copyrightDE = " | Copyright &#169; 2022 Lucas Urbain | Alle Rights Vorbehalten.";
     var copyrightFR = " | Copyright &#169; 2022 Lucas Urbain | Tous Droits Réservés.";
