7eb61080be2406626cf4325b92245d4f5e4e450f
lucasurbain <lucasurbain68@protonmail.ch>
2023-01-11 15:58:39 +0100

    2023.11.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,10 +146,10 @@ window.addEventListener('scroll', scrollTop)
 
 /*=============== GET VERSION ===============*/
 function getVersion() {
-    var version = "2023.11.1";
-    var copyright = " | Copyright &#169; 2022 Lucas Urbain | All Rigths Reserved.";
-    var copyrightDE = " | Copyright &#169; 2022 Lucas Urbain | Alle Rights Vorbehalten.";
-    var copyrightFR = " | Copyright &#169; 2022 Lucas Urbain | Tous Droits Réservés.";
+    var version = "2023.11.2";
+    var copyright = " | Copyright &#169; 2023 Lucas Urbain | All Rigths Reserved.";
+    var copyrightDE = " | Copyright &#169; 2023 Lucas Urbain | Alle Rights Vorbehalten.";
+    var copyrightFR = " | Copyright &#169; 2023 Lucas Urbain | Tous Droits Réservés.";
 
     var language = document.getElementsByClassName("footer__copy");
 
