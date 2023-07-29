7fdc3902dfa9699d7d59f173166461c518f914f0
lucasurbain <lucasurbain68@protonmail.ch>
2023-07-29 13:26:13 +0200

    2023.23.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,7 +162,7 @@ window.addEventListener('scroll', scrollTop)
 
 /*=============== GET VERSION ===============*/
 function getVersion() {
-    var version = "2023.23.1";
+    var version = "2023.23.2";
     var copyright = " | Copyright &#169; 2023 Lucas Urbain | All Rigths Reserved.";
     var copyrightDE = " | Copyright &#169; 2023 Lucas Urbain | Alle Rights Vorbehalten.";
     var copyrightFR = " | Copyright &#169; 2023 Lucas Urbain | Tous Droits Réservés.";
